b88297dad08d7072c98afa2a3ce3d389ab153b32
Nils Westhoff <nwesthoff@conceptivate.nl>
2019-05-31 09:22:28 +0200

    🎉 First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# VanBerlo ADFS Passport Strategy
+
+This is an [Microsoft ADFS](http://en.wikipedia.org/wiki/SAML_2.0) authentication provider for [Passport](http://passportjs.org/), the Node.js authentication library. We can use it to enable SSO in VanBerlo applications.
+
+## Installation
+
+    $ npm install https://github.com/nwesthoff/VanBerlo-ADFS-Passport-Strategy
+
+## Usage & Examples
+
+WIP 🚧
---
+++ src/strategy.ts
@@ -0,0 +1,23 @@
+import OAuth2 from "passport-oauth2";
+import { decode } from "jsonwebtoken";
+import md5 from "md5";
+
+class ADFSAuthStrategy extends OAuth2 {
+  userProfile = function(accessToken, done) {
+    const decoded = decode(accessToken);
+
+    return done(null, {
+      name: decoded.unique_name,
+      email: decoded.email,
+      id: md5(decoded.email)
+    });
+  };
+
+  authorizationParams = function(options) {
+    return {
+      resource: options.resource
+    };
+  };
+}
+
+export default ADFSAuthStrategy;
